a73a50b2e3d2e7be7214cecd01daef0b0a208953
jeanbejar <jeanevabejar@gmail.com>
2024-03-07 20:37:28 +0800

    refactor: put restriction to login and registration page.

--- src/Pages/Signin.jsx
+++ src/Pages/Signin.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import LoginForm from "../Components/LoginForm";
+import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
 
 const Signin = () => {
+  const user = Cookies.get("token");
+  const nav = useNavigate();
+
   return (
-<div className=" layersize display ">
-  <div className=" display w-[50%] h-[50vh] rounded-2xl bg-white drop-shadow-2xl"> 
-     <LoginForm/>
-  </div>
- 
-</div>
+    user ? (
+     nav("/")
+    ) : (
+      <div className="layersize display">
+        <div className="display w-[50%] h-[50vh] rounded-2xl bg-white drop-shadow-2xl"> 
+          <LoginForm/>
+        </div>
+      </div>
+    )
   );
+  
 };
 
 export default Signin;
--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import RegisterForm from "../Components/RegisterForm";
-
+import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
-  return (
+  const user = Cookies.get("token");
+  const nav = useNavigate();
+  return user ? (
+    nav("/")
+  ) : (
     <div className=" layersize display ">
-      <div className=" display w-[50%] h-[50vh] rounded-2xl bg-white drop-shadow-2xl"> 
-         <RegisterForm/>
+      <div className=" display w-[50%] h-[50vh] rounded-2xl bg-white drop-shadow-2xl">
+        <RegisterForm />
       </div>
-     
     </div>
-      );
+  );
 };
 
 export default Signup;
