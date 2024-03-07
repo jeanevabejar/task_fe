7b10337c33086f08720f8b3b5016634d6c439d95
jeanbejar <jeanevabejar@gmail.com>
2024-03-07 21:03:50 +0800

    fix: bug problem.

--- src/Components/ProtectedRoute.jsx
+++ src/Components/ProtectedRoute.jsx
@@ -1,18 +1,12 @@
 import { redirect } from "react-router-dom";
 import { isAuthenticated } from "../Utils/Authservice";
 
-import { useLocation} from "react-router-dom";
 
 const ProtectedRoute = () => {
   const isLoggedIn = isAuthenticated();
-  const location = useLocation();
 
   if (isLoggedIn === false) {
     return redirect("/signin");
-  }
-
-  if (isLoggedIn && location.pathname === "/signin" || location.pathname === "/signup" ) {
-  return  redirect("/");
   }
 
   return null;
