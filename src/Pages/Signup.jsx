944a738036fd8c94344b504e974684ddcfc92c1e
jeanbejar <jeanevabejar@gmail.com>
2024-03-07 20:56:57 +0800

    fix:missing file.

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,16 +1,9 @@
 import React from "react";
 import RegisterForm from "../Components/RegisterForm";
-import Cookies from "js-cookie";
-import { useLocation, useNavigate } from "react-router-dom";
+
 
 const Signup = () => {
-  const user = Cookies.get("token");
-  const nav = useNavigate();
-  const location = useLocation();
-
-  if (user && location.pathname === "/signin" || location.pathname === "/signup" ) {
-    nav("/");
-  }
+ 
   return (
     <div className=" layersize display ">
       <div className=" display w-[50%] h-[50vh] rounded-2xl bg-white drop-shadow-2xl">
